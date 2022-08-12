0f3845aeb2c52dd6dc6d5ab776757c348734e720
RichardMartinAUS <richard.martin001@gmail.com>
2022-08-12 16:35:40 +1000

    updated schema file

--- db/schema.sql
+++ db/schema.sql
@@ -4,30 +4,30 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  department_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
     FOREIGN KEY (department_id)
-    REFERENCES department(id)
+    REFERENCES department(department_id)
     ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     FOREIGN KEY (role_id)
-    REFERENCES role(id)
+    REFERENCES role(role_id)
     ON DELETE CASCADE,
     manager_id INT,
     FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
+    REFERENCES employee(employee_id)
     ON DELETE SET NULL
 );
